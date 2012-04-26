e929003e3048c74535c6d57e35e8af8c04595b9d
Matthew Kitt <mk.kitt@gmail.com>
2012-04-25 22:24:56 -0600

    The whole kit and kaboodle

--- lib/sherpa.rb
+++ lib/sherpa.rb
@@ -3,16 +3,16 @@ require 'json'
 require './lib/sherpa/builder'
 
 module Sherpa
-  # files = Dir["./app/assets/stylesheets/*.sass"]
+  files = Dir["./app/assets/stylesheets/*.sass"]
   # files = [
     # "./app/assets/stylesheets/breadcrumbs.sass",
     # "./app/assets/stylesheets/visibility.sass",
     # "./app/assets/stylesheets/box-sizing.sass"
   # ]
 
-  files = [
-    "./app/assets/stylesheets/font-size.sass"
-  ]
+  # files = [
+    # "./app/assets/stylesheets/font-size.sass"
+  # ]
 
   builder = Builder.new(files)
   json = JSON.pretty_generate(builder.build)
