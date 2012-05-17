5a62016a49d93d99ec6a5c418e709adcadcd9ef7
Matthew Kitt <mk.kitt@gmail.com>
2012-05-17 14:07:43 -0600

    downcast those test file names

--- test/helper.rb
+++ test/helper.rb
@@ -4,7 +4,7 @@ require 'sherpa'
 module Sherpa
   class Test < MiniTest::Unit::TestCase
     def self.test(name, &block)
-      define_method("test_#{name.gsub(/\W/,'_')}", &block) if block
+      define_method("test_#{name.gsub(/\W/,'_').downcase}", &block) if block
     end
 
     def default_test
